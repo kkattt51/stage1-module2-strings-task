229cf5e3d7931f082c033cc9f05ba1e8568f1b53
Kislitskaya_I <IKislitskaya@ibagroup.eu>
2022-11-07 16:50:35 +0300

    stage1 module2 string task

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,56 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String accessModifier;
+        String returnType;
+        String methodName;
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+        String[] splitByBraces = signatureString.split("[()]");
+        String[] splitLeftPart = splitByBraces[0].split(" ");
+        if (splitLeftPart.length == 3) {
+            accessModifier = splitLeftPart[0];
+            returnType = splitLeftPart[1];
+            methodName = splitLeftPart[2];
+        } else {
+            accessModifier = null;
+            returnType = splitLeftPart[0];
+            methodName = splitLeftPart[1];
+        }
+        if (splitByBraces.length != 1) {
+            String[] splitRightPart = splitByBraces[1].split(", ");
+            for (int i = 0; i < splitRightPart.length; i++) {
+                String[] typeAndName = splitRightPart[i].split(" ");
+                String type = typeAndName[0];
+                String name = typeAndName[1];
+                MethodSignature.Argument argument = new MethodSignature.Argument(type, name);
+                arguments.add(argument);
+            }
+        }
+        MethodSignature resultObject = new MethodSignature(methodName, arguments);
+        resultObject.setAccessModifier(accessModifier);
+        resultObject.setReturnType(returnType);
+        return resultObject;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -8,11 +8,21 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder sb = new StringBuilder("[");
+        for (String delimiter : delimiters) {
+            sb.append(delimiter);
+        }
+        sb.append("]+");
+        String regEx = sb.toString();
+        if (source.matches(regEx + ".*")) {
+            return List.of(source.replaceFirst(regEx, "").split(regEx));
+        } else {
+            return List.of(source.split(regEx));
+        }
     }
 }
